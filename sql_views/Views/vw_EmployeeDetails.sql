ec8db819fd281647bc01600265fa6304959ef521
Abishek-Palanisamy2 <abishek.palanisamy@cannytechsolutions.com>
2026-07-27 10:39:16 +0530

    modified

--- sql_views/Views/vw_EmployeeDetails.sql
+++ sql_views/Views/vw_EmployeeDetails.sql
@@ -7,8 +7,4 @@ SELECT
     d.department_name
 FROM employee e
 JOIN department d
-<<<<<<< HEAD
-ON e.department_id = d.deparent_id;
-=======
-ON e.department_id = d.deparment_id;
->>>>>>> 505dfd7867965af02e80e5818dfcd6de4376871a
+ON e.department_id = d.depament_id;
