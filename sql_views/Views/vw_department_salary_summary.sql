45f4f113aeb6bc262280bd1aa376b2831e342268
Abishek-Palanisamy2 <abishek.palanisamy@cannytechsolutions.com>
2026-07-27 12:20:52 +0530

    new views changes made

--- sql_views/Views/vw_department_salary_summary.sql
+++ sql_views/Views/vw_department_salary_summary.sql
@@ -7,5 +7,5 @@ SELECT
     AVG(e.salary) AS average_salary
 FROM department d
 LEFT JOIN employee e
-ON d.depament_id = e.department_id
+ON d.department_id = e.department_id_
 GROUP BY d.department_id, d.department_name;
